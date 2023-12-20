6b1637d8f1b5c9e7ca2c2534c6f06713a5c235c5
erischon <66017574+erischon@users.noreply.github.com>
2023-12-20 14:54:55 +0100

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,27 +3,35 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="p-4 h-[196px]">
-      <div className="flex h-full">
-        <div className="flex flex-col justify-between">
-          <h2>Title</h2>
+    <section className="p-4 h-[300px] overflow-hidden bg-lime-50">
+      <div className="flex h-full justify-between">
+        <div className="flex flex-col justify-between w-1/3">
+          <h3 className="text-sm">Title</h3>
 
-          <div>
-            <p>subtitle</p>
+          <div className="">
+            <h2 className="text-xl font-bold">
+              Votre électricien prêt de chez vous
+            </h2>
+
+            <p className="text-xs">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error ad
+              esse eum unde quasi magnam eligendi quibusdam totam dolorem
+              corporis exercitationem
+            </p>
 
             <Link href="">link</Link>
           </div>
         </div>
 
-        <div>
+        <div className="flex justify-end items-start w-2/3">
           <Image
             src="/images/Electrician-pana.png"
-            width={200}
-            height={200}
+            width={300}
+            height={300}
             alt="hero"
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
